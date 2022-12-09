ef554a6924cd95fd7b98b5f8a7742cc08a08a0b6
Muhammad Elkfrawy <kfrawee@outlook.com>
2022-12-09 22:07:13 +0200

    update

--- layers/pybinance/python/pybinance/utils/telegram_bot.py
+++ layers/pybinance/python/pybinance/utils/telegram_bot.py
@@ -1,7 +1,11 @@
 import os
+from typing import Dict
 
 from telegram import Bot, ParseMode
-from telegram.error import RetryAfter, TimedOut
+
+# from telegram.error import RetryAfter, TimedOut
+
+from .helpers import logger
 
 TELEGRAM_BOT_API_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
@@ -12,13 +16,13 @@ class TelegramBot:
         self.bot_client = Bot(TELEGRAM_BOT_API_KEY)
         self.chat_id = TELEGRAM_CHAT_ID
         self.emojis = {
-            "ALERT": "⚠️⚠️",
-            "ERROR": "❌❌",
-            "SUCCESS": "✅✅",
-            "ADD": "🆕✨",
-            "UPDATE": "🔃✨",
-            "UP": "⬆️📈",
-            "DOWN": "⬇️📉",
+            "ALERT": "⚠️",
+            "ERROR": "❌",
+            "SUCCESS": "✅",
+            "LONG": "🟢",
+            "SHORT": "🔴",
+            "UP": "⬆️",
+            "DOWN": "⬇️",
         }
 
     def _send_message(
@@ -40,17 +44,19 @@ class TelegramBot:
 
             pattern = re.compile(r"<.*?>")
             message_ = pattern.sub("", message)
-            print(message_)
+            # print(message_)
+            logger.debug(message_)
 
-        if emoji in self.emojis.keys():
-            message = f"{self.emojis.get(emoji)}  {message}"
+        # if emoji in self.emojis.keys():
+        #     message = f"{self.emojis.get(emoji)}  {message}"
 
         try:
             self.bot_client.send_message(
                 chat_id=self.chat_id, text=f"{message}", parse_mode=ParseMode.HTML
             )
         except Exception as e:
-            print(f"Error sending message, '{e.message}'. Retrying in 1 second.")
+            # print(f"Error sending message, '{e.message}'. Retrying in 1 second.")
+            logger.debug(f"Error sending message, '{e.message}'. Retrying in 1 second.")
 
             # maybe timeout - try to cool down
             from time import sleep
@@ -61,93 +67,112 @@ class TelegramBot:
                     chat_id=self.chat_id, text=f"{message}", parse_mode=ParseMode.HTML
                 )
             except Exception as e:  # -_-
-                print(f"Error sending message again, '{e.message}'.")
+                # print(f"Error sending message again, '{e.message}'.")
+                logger.debug(f"Error sending message again, '{e.message}'.")
 
-    def send_new_item_added(
-        self, item_title: str, item_url: str, item_price: float
-    ) -> None:
+    # def send_new_item_added(
+    #     self, item_title: str, item_url: str, item_price: float
+    # ) -> None:
+    #     """
+    #     Send message with new item added.
+
+    #     Args:
+    #         item_title (str):  name of the item.
+    #         item_url (str): url of the item.
+    #         item_price (float): price of the item.
+    #     Returns:
+    #         None
+    #     """
+    #     emoji = "ADD"
+    #     message = (
+    #         f"<b>Hey! A new item was added!</b>\n"
+    #         f"<a href='{item_url}'>{item_title}</a> - <b>Price:</b> ${item_price}\n"
+    #     )
+
+    #     self._send_message(message=message, emoji=emoji)
+
+    # def send_new_items_added(self, items_count: int) -> None:
+    #     """
+    #     Send message with new items added.
+
+    #     Args:
+    #         items_count (int):  number of newly added items.
+    #     Returns:
+    #         None
+    #     """
+    #     emoji = "ADD"
+    #     message = f"<b>Hey! New {items_count} items were added!</b>\n"
+
+    #     self._send_message(message=message, emoji=emoji, stdout=True)
+
+    # def send_new_items_updated(self, items_count: int) -> None:
+    #     """
+    #     Send message with new items updated.
+
+    #     Args:
+    #         items_count (int):  number of newly updated items.
+    #     Returns:
+    #         None
+    #     """
+    #     emoji = "UPDATE"
+    #     message = f"<b>Hey! New {items_count} items were updated!</b>\n"
+
+    #     self._send_message(message=message, emoji=emoji, stdout=True)
+
+    # def send_price_update(
+    #     self,
+    #     item_title: str,
+    #     item_url: str,
+    #     item_old_price: float,
+    #     item_new_price: float,
+    #     send_all_updates: bool,
+    # ) -> None:
+    #     """
+    #     Send message if item's price is updated.
+
+    #     Args:
+    #         item_title (str): name of the item.
+    #         item_url (str): url of the item.
+    #         item_old_price (float): old price of the item.
+    #         item_new_price (float): new price of the item.
+    #         send_all_updates (bool): Either to send all updated prices (increased or decreased) or only the decreased prices.
+    #     Returns:
+    #         None
+    #     """
+    #     if item_old_price == item_new_price:
+    #         return
+    #     elif item_old_price > item_new_price:
+    #         emoji = "DOWN"
+    #     else:
+    #         emoji = "UP"
+    #         if not send_all_updates:
+    #             return
+
+    #     message = (
+    #         "<b>Hey! An item's price has been updated!</b> \n"
+    #         f"<a href='{item_url}'>{item_title}</a> \n"
+    #         f"<b>Old Price:</b> ${item_old_price} - <b>New Price:</b> ${item_new_price}"
+    #     )
+
+    #     self._send_message(message=message, emoji=emoji)
+
+    def send_summary(self, data: Dict) -> None:
         """
-        Send message with new item added.
+        Send summary of the data.
 
         Args:
-            item_title (str):  name of the item.
-            item_url (str): url of the item.
-            item_price (float): price of the item.
+            data (Dict): raw data prepare and send.
         Returns:
             None
         """
-        emoji = "ADD"
+
+        users_data = data.get("data")
+
         message = (
-            f"<b>Hey! A new item was added!</b>\n"
-            f"<a href='{item_url}'>{item_title}</a> - <b>Price:</b> ${item_price}\n"
+            "<b> Top 10 Traders by Daily PNL </b>\n"
+            f"<h4>Datetime: {data.get('datetime')}</h4>"
         )
-
-        self._send_message(message=message, emoji=emoji)
-
-    def send_new_items_added(self, items_count: int) -> None:
-        """
-        Send message with new items added.
-
-        Args:
-            items_count (int):  number of newly added items.
-        Returns:
-            None
-        """
-        emoji = "ADD"
-        message = f"<b>Hey! New {items_count} items were added!</b>\n"
-
-        self._send_message(message=message, emoji=emoji, stdout=True)
-
-    def send_new_items_updated(self, items_count: int) -> None:
-        """
-        Send message with new items updated.
-
-        Args:
-            items_count (int):  number of newly updated items.
-        Returns:
-            None
-        """
-        emoji = "UPDATE"
-        message = f"<b>Hey! New {items_count} items were updated!</b>\n"
-
-        self._send_message(message=message, emoji=emoji, stdout=True)
-
-    def send_price_update(
-        self,
-        item_title: str,
-        item_url: str,
-        item_old_price: float,
-        item_new_price: float,
-        send_all_updates: bool,
-    ) -> None:
-        """
-        Send message if item's price is updated.
-
-        Args:
-            item_title (str): name of the item.
-            item_url (str): url of the item.
-            item_old_price (float): old price of the item.
-            item_new_price (float): new price of the item.
-            send_all_updates (bool): Either to send all updated prices (increased or decreased) or only the decreased prices.
-        Returns:
-            None
-        """
-        if item_old_price == item_new_price:
-            return
-        elif item_old_price > item_new_price:
-            emoji = "DOWN"
-        else:
-            emoji = "UP"
-            if not send_all_updates:
-                return
-
-        message = (
-            "<b>Hey! An item's price has been updated!</b> \n"
-            f"<a href='{item_url}'>{item_title}</a> \n"
-            f"<b>Old Price:</b> ${item_old_price} - <b>New Price:</b> ${item_new_price}"
-        )
-
-        self._send_message(message=message, emoji=emoji)
+        self._send_message(message=message)
 
     def send_alert(self, message: str) -> None:
         """
