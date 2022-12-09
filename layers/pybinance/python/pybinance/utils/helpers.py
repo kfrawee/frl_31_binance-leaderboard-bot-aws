05fd49291508dc7d01121f1e7ce64ed61bb3d9b9
Muhammad Elkfrawy <kfrawee@outlook.com>
2022-12-09 17:51:30 +0200

    Update helpers.py

--- layers/pybinance/python/pybinance/utils/helpers.py
+++ layers/pybinance/python/pybinance/utils/helpers.py
@@ -1,6 +1,7 @@
 """ Helpers utils for the bot"""
 import os
 import traceback
+from datetime import datetime
 from http import HTTPStatus
 from typing import Optional, Dict, List
 
@@ -21,6 +22,22 @@ headers = {
     "Content-Type": "application/json",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
 }
+
+
+def extract_datetime(timestamp: int) -> str:
+    """
+    Extract datetime from timestamp.
+
+    Args:
+        timestamp (int): Timestamp.
+
+    Returns:
+        datetime (str): Datetime.
+    """
+    if len(timestamp) > 10:
+        timestamp = timestamp // 1000  # remove ms
+
+    return str(datetime.fromtimestamp(timestamp))
 
 
 def get_leader_board_rank() -> Optional[List[Dict]]:
@@ -94,7 +111,7 @@ def get_trader_performance(encryptedUid: str) -> Dict:
     Returns:
         performance_data (Dict): Trader performance data.
     """
-    url = "https://www.binance.com/bapi/futures/v3/public/future/leaderboard/getLeaderboardRank"
+    url = "https://www.binance.com/bapi/futures/v2/public/future/leaderboard/getOtherPerformance"
     request_body = {
         "encryptedUid": encryptedUid,
         "tradeType": "PERPETUAL",
@@ -170,22 +187,121 @@ def get_trader_positions(encryptedUid: str) -> Dict:
     return date
 
 
-def generate_user_data(rank_data: Dict, performance_data: Dict, position_data: Dict) -> Dict:
+def clean_performance_data(performance_data: Dict) -> Dict:
     """
-    Generate full trader data; rank, performance and positions data.
+    Clean performance data.
 
     Args:
-        rank_data (str): Trader rank data.
         performance_data (Dict): Trader performance data.
-        positions_data (Dict): List of trader positions data.
 
     Returns:
-        user_data (Dict): Trader's data.
+        cleaned_performance_data (Dict): Cleaned trader performance data.
+    """
+
+    cleaned_performance_data = {
+        "Last Trade": extract_datetime(performance_data.get("lastTradeTime")),
+    }
+
+    for data in performance_data.get("performanceRetList"):
+        if data.get("periodType") == "DAILY":
+            if data.get("statisticsType") == "ROI":
+                cleaned_performance_data.update(
+                    Daily_ROI=round(data.get("value")),
+                )
+            elif data.get("statisticsType") == "PNL":
+                cleaned_performance_data.update(
+                    Daily_PNL=round(data.get("value")),
+                )
+        if data.get("periodType") == "WEEKLY":
+            if data.get("statisticsType") == "ROI":
+                cleaned_performance_data.update(
+                    Weekly_ROI=round(data.get("value")),
+                )
+            elif data.get("statisticsType") == "PNL":
+                cleaned_performance_data.update(
+                    Weekly_PNL=round(data.get("value")),
+                )
+        if data.get("periodType") == "MONTHLY":
+            if data.get("statisticsType") == "ROI":
+                cleaned_performance_data.update(
+                    Monthly_ROI=round(data.get("value")),
+                )
+            elif data.get("statisticsType") == "PNL":
+                cleaned_performance_data.update(
+                    Monthly_PNL=round(data.get("value")),
+                )
+        if data.get("periodType") == "ALL":
+            if data.get("statisticsType") == "ROI":
+                cleaned_performance_data.update(
+                    All_ROI=round(data.get("value")),
+                )
+            elif data.get("statisticsType") == "PNL":
+                cleaned_performance_data.update(
+                    All_PNL=round(data.get("value")),
+                )
+
+    return cleaned_performance_data
+
+
+def clean_positions_data(positions_data: Dict) -> Dict:
+    """
+    Clean positions data.
+
+    Args:
+        positions_data (Dict): Trader positions data.
+
+    Returns:
+        cleaned_positions_data (Dict): Cleaned trader positions data.
     """
     # amount +ve short
     # amount -ve long
 
-    return
+    cleaned_positions_data = {
+        "Last Update": extract_datetime(positions_data.get("updateTimeStamp")),
+    }
+    
+    cleaned_positions_data.update(
+        Positions=[
+            {
+                "Symbol": data.get("symbol"),
+                "leverage": data.get("leverage"),
+                "type": "Long" if data.get("amount") > 0 else "Short",
+                "entryPrice": data.get("entryPrice"),
+                "marketPrice": data.get("marketPrice"),
+                "PNL": data.get("pnl"),
+                "POE": data.get("roe"),
+                "datetime": extract_datetime(data.get("updateTimeStamp")),
+            }
+            for data in positions_data.get("otherPositionRetList")
+        ]
+    )
+
+    return cleaned_positions_data
+
+
+def generate_user_data(
+    trader_rank: int, rank_data: Dict, performance_data: Dict, position_data: Dict
+) -> Dict:
+    """
+    Generate full trader data; rank, performance and positions data.
+
+    Args:
+        trader_rank (int): Trader rank from 1 to 10.
+        rank_data (str): Trader raw rank data.
+        performance_data (Dict): Trader raw performance data.
+        positions_data (Dict): List of trader raw positions data.
+
+    Returns:
+        user_data (Dict): Trader's data.
+    """
+
+    return {
+        "rank": trader_rank,
+        "name": rank_data.get("nickName"),
+        "encryptedUid": rank_data.get("encryptedUid"),
+        "performance": clean_performance_data(performance_data),
+        "positions": clean_positions_data(position_data),
+    }
 
 
 if __name__ == "__main__":
